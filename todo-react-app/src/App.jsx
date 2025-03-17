ca6d84d5c1e1a5342b02fdfc5ac0ba9e1854d4c7
Christos Sykas <dev.chsykas@gmail.com>
2025-03-17 23:52:01 +0200

    `Added basic UI components for To-Do List App`

--- todo-react-app/src/App.jsx
+++ todo-react-app/src/App.jsx
@@ -1,7 +1,26 @@
+import { Fragment } from "react";
 import "./App.css";
 
 function App() {
-  return;
+  return (
+    <Fragment>
+      <section className="todo-container">
+        <h1>To-Do List App</h1>
+        <div className="todo-input">
+          <input type="text" placeholder="Enter"></input>
+          <button>Add</button>
+        </div>
+        <div className="todo-list-container">
+          <h3 className="todo-list-title">today's tasks</h3>
+          <ul className="todo-list">
+            <li className="todo-list-item">Task 1</li>
+            <li className="todo-list-item">Task 2</li>
+            <li className="todo-list-item">Task 3</li>
+          </ul>
+        </div>
+      </section>
+    </Fragment>
+  );
 }
 
 export default App;
