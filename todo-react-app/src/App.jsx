852b2cfadf5d616f38a1eb5bd5c33d33f27a4aaa
Christos Sykas <dev.chsykas@gmail.com>
2025-03-18 01:26:25 +0200

    `Added Font Awesome icons and updated styles for Todo List App`

--- todo-react-app/src/App.jsx
+++ todo-react-app/src/App.jsx
@@ -5,17 +5,26 @@ function App() {
   return (
     <Fragment>
       <section className="todo-container">
-        <h1>To-Do List App</h1>
-        <div className="todo-input">
-          <input type="text" placeholder="Enter"></input>
-          <button>Add</button>
+        <h1 className="todo-title">ToDo List App</h1>
+        <div className="todo-input-container">
+          <input className="todo-input" type="text" placeholder="Enter"></input>
+          <button className="todo-add-btn">Add</button>
         </div>
         <div className="todo-list-container">
           <h3 className="todo-list-title">today's tasks</h3>
           <ul className="todo-list">
-            <li className="todo-list-item">Task 1</li>
-            <li className="todo-list-item">Task 2</li>
-            <li className="todo-list-item">Task 3</li>
+            <li className="todo-list-item">
+              <i className="fa-regular fa-circle"></i>
+              Task 1
+            </li>
+            <li className="todo-list-item">
+              <i className="fa-regular fa-circle"></i>
+              Task 2
+            </li>
+            <li className="todo-list-item">
+              <i className="fa-regular fa-circle"></i>
+              Task 3
+            </li>
           </ul>
         </div>
       </section>
